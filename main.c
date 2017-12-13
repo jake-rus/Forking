ccab9514bb00a41a87463cfc18e19c166ddbd33a
jake-rus <34259491+jake-rus@users.noreply.github.com>
2017-12-13 18:43:10 +0700

    Update main.c

--- main.c
+++ main.c
@@ -7,25 +7,34 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
 
-void printProc(int n)
-{ printf("I'm Process %d (pid %d; ppid %d)\n", n++, getpid(), getppid());}
 
-int main (void) {
-        pid_t p2=1,p3=1,p4=1,p5=1,p6=1; // тип для возвращаемого PID
-        int n=1;
-        printProc(n);
-        p2 = fork();
-        if (p2 ==0)	{printProc(n);p4 = fork();}
-        else { wait(p2); p3 = fork();}
-        if (p3 ==0) {
-        	printProc(n);
-        	p5 = fork();
-        	p6 = fork();}
+ int main(int argc, char **argv)
+  {
+	 char n[]="1";
+	 pid_t p2,p3,p4,p5,p6;
 
-        	if (p5 ==0) printProc(n);
-        	if (p6 ==0) { wait (p5);printProc(n);}
-        return 0;
+	 char *arg[] = {"child","1",0};
 
+	 printf("parent (pid %d - ppid %d)\n", getpid(),getppid());
+
+	 if ((p2=fork()) ==0){
+		 p4=fork(); // создание 4 процесса под вторым
+		 if (p4 > 1) waitpid(p4); // 2 процесс ждет завершения 4
+		 if (execv(arg[0], arg)==-1) perror("exec failed");}
+
+	 if ((p3=fork()) ==0)
+	 {
+		 p5=fork(); // создание 5 процесса под 3
+		 if (p5 > 1) {
+			 waitpid(p5); // 3 процесс ждет завершения 5
+		 	 p6=fork(); // создание 6 процесса под 3
+		 	 if (p6 > 1) waitpid(p6);} // 3 процесс ждет завершения 6
+		 if (execv(arg[0], arg)==-1) perror("exec failed");
+	 }
+
+	 if (p2>1) waitpid(p2); // 1 процесс ждет завершения 2
+	 if (p3>1) waitpid(p3); // 1 процесс ждет завершения 3
+	 return 0;
 }
-
